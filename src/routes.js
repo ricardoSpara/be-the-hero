0f354b3a9805a7ef4e5a28b2d12f034088b61dd6
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-03-24 21:53:17 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,19 @@ const { Router } = require("express");
 const routes = new Router();
 
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
 
 module.exports = routes;
