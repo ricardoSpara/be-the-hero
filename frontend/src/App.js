d12c78ed5b269848092707148ee3ad4105d9e141
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-03-28 18:57:28 -0300

    Logon page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Logon from "./pages/Logon";
 
+import "./global.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Omnistack.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Logon></Logon>;
 }
 
 export default App;
